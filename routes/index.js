4fa15385c01428ec5b742e696d0373073abc1351
ENAwareness <166632095+ENAwareness@users.noreply.github.com.>
2025-06-09 01:50:37 +0900

    Add routes for index ("/") and new message form ("/new")

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  { text: 'Hello World!', user: 'Alice', added: new Date() },
+  { text: 'Hi,there!', user: 'Bob', added: new Date() }
+];
+
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Mini Message Board' });
+  res.render('index', { title: 'Mini Message Board', messages });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form', { title: 'Add New Message' });
+});
+
+router.post('/new', (req, res) => {
+  const { user, message } = req.body;
+  messages.push({ text: message, user: user, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
